e14d89356430db5baba9dbc53340988b519195ed
mendesjorge <luis.7win@gmail.com>
2017-01-31 23:30:03 +0000

    Added multi language package universal.i18n align nav bar properly reorganized some code

--- shuteye/imports/ui/App.jsx
+++ shuteye/imports/ui/App.jsx
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
  
-import Task from './Task.jsx';
+import i18n from 'meteor/universe:i18n';
+import MenuBar from './MenuBar';
+// import Task from './Task.jsx';
  
 // App component - represents the whole app
 export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
- 
-  renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
- 
+
   render() {
     return (
       <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
- 
-        <ul>
-          {this.renderTasks()}
-        </ul>
+        <MenuBar/>
       </div>
     );
   }
--- shuteye/imports/ui/MenuBar.jsx
+++ shuteye/imports/ui/MenuBar.jsx
@@ -1,9 +1,47 @@
 import React, { Component, PropTypes } from 'react';
 
+import i18n from 'meteor/universe:i18n';
+//import LanguageStrings from './LanguageStrings';
+
 export default class MenuBar extends Component {
-  render() {
-    return (
-      < title={this.props.task._id}>{this.props.task.text}</li>
-    );
+  
+	
+  	render() {
+	  	const T = i18n.createComponent();
+	  	i18n.setLocale(getLang());
+	    return (
+	    	<nav classname="navbar">
+				<div classname="container">
+					<a classname="navbar-brand" href="#">
+						<T>common.navbar.shuteye</T>
+					</a>
+
+					<div classname="collapse navbar-collapse">
+						<ul classname="navbar-nav mr-auto">
+							<li classname="nav-item active">
+								<a classname="nav-link" href="#">
+									<T>common.navbar.HowItWorks</T> 
+								</a>
+							</li>
+							<li classname="nav-item">
+								<a classname="nav-link" href="#">
+									<T>common.navbar.WhyShuteye</T>
+								</a>
+							</li>
+							<li classname="nav-item">
+								<a classname="nav-link" href="#">
+									<T>common.navbar.HowIStart</T>
+								</a>
+							</li>
+							<li classname="nav-item">
+								<a classname="nav-link" href="#">
+									<T>common.navbar.WhatIsNew</T>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+	    );
   }
 }
