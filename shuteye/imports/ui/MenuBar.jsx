73d3fa7e955b70807e13709ea5f710e097bfa437
mendesjorge <luis.7win@gmail.com>
2017-03-18 18:34:06 +0000

    Changed color, added logo and some design content

--- shuteye/imports/ui/MenuBar.jsx
+++ shuteye/imports/ui/MenuBar.jsx
@@ -13,14 +13,14 @@ export default class MenuBar extends Component {
 	    	<nav className="navbar navbar-fixed-top">
 				<div className="pn-container">
 					<div className="nav-header">
-						<a className="navbar-brand" href="#index">
-							<T>common.navbar.shuteye</T>
+						<a className="navbar-brand logo" href="#index">
+							<div/>
 						</a>
 						<LanguageDrop/>
 					</div>
 					<div className="collapse navbar-collapse">
 						<ul className="navbar-nav mr-auto">
-							<li className="nav-item active">
+							<li className="nav-item">
 								<a className="nav-link" href="#how">
 									<T>common.navbar.HowItWorks</T> 
 								</a>
