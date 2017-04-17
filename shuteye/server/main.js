b699031b7fdb34769ba407b83e54a75ffa176d9a
mendesjorge <luis.7win@gmail.com>
2017-04-17 23:46:43 +0100

    Moved x-ua-compatible to response header, fixed some IE 11 problems

--- shuteye/server/main.js
+++ shuteye/server/main.js
@@ -2,6 +2,11 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/tasks.js';
 
+var connectHandler = WebApp.connectHandlers;
+
 Meteor.startup(() => {
-  // code to run on server at startup
+	connectHandler.use((req,res,next) => {
+		res.setHeader("X-UA-Compatible","IE=edge");
+		return next();
+	});
 });
